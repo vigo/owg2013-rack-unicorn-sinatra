0059860d8afad899ea4207af9c6b06669e046755
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2013-11-19 17:31:42 +0200

    install scripts

---
+++ scripts/install_rbenv_ubuntu_12_04.sh
@@ -0,0 +1,15 @@
+sudo aptitude update
+sudo aptitude install zlib1g-dev openssl libopenssl-ruby1.9.1 libssl-dev libruby1.9.1 libreadline-dev build-essential
+if [[ ! `which git` ]]; then
+  sudo aptitude install git-core
+fi
+cd
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+mkdir -p ~/.rbenv/plugins
+cd ~/.rbenv/plugins
+git clone git://github.com/sstephenson/ruby-build.git
+rbenv install 1.9.3-p448
+rbenv rehash
+gem install bundler
+rbenv rehash
