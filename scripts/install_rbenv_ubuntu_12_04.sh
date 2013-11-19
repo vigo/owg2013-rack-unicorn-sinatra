d6408c00298db303354f3fcc09bf73f5fd7f4f5f
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2013-11-19 17:37:06 +0200

    bash script fixed

--- scripts/install_rbenv_ubuntu_12_04.sh
+++ scripts/install_rbenv_ubuntu_12_04.sh
@@ -10,6 +10,8 @@ fi
 cd
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
 mkdir -p ~/.rbenv/plugins
 cd ~/.rbenv/plugins
 git clone git://github.com/sstephenson/ruby-build.git
