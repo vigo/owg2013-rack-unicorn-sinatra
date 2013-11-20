c9d756e1c966d7feb22fe9953c9fc80ff5cb38db
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2013-11-20 19:04:51 +0200

    installer script update

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,10 +3,10 @@
 set -e
 
 echo "Installing rbenv, ruby 1.9.3-p448 and related gems..."
-source ./install_rbenv_ubuntu_12_04.sh
+source /home/vagrant/owg2013-rack-unicorn-sinatra/scripts/install_rbenv_ubuntu_12_04.sh
 
 echo "Installing nginx and configuring needs..."
-source ./install_nginx.sh
+source /home/vagrant/owg2013-rack-unicorn-sinatra/scripts/install_nginx.sh
 
 if [[ ! -e ~/.ackrc ]]; then
     sudo ln -s /home/vagrant/owg2013-rack-unicorn-sinatra/sinatra-nginx-unicorn/service.sh /etc/init.d/unicornd_my_unicorn_application
